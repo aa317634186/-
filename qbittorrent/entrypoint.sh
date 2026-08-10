5f98c10d238cc37d6e96835507b584ff058da666
aa317634186 <aa317634186@users.noreply.github.com>
2026-08-10 12:26:53 +0800

    Allow internal qBittorrent API access

--- qbittorrent/entrypoint.sh
+++ qbittorrent/entrypoint.sh
@@ -6,4 +6,10 @@ if [ ! -f /config/qBittorrent/qBittorrent.conf ]; then
   cp /defaults/qBittorrent.conf /config/qBittorrent/qBittorrent.conf
 fi
 
+conf=/config/qBittorrent/qBittorrent.conf
+for setting in 'WebUI\\HostHeaderValidation=false' 'WebUI\\OriginHeaderValidation=false' 'WebUI\\AuthenticationSubnetWhitelistEnabled=false'; do
+  key=${setting%%=*}
+  grep -Fq "${key}=" "$conf" || printf '\n%s\n' "$setting" >> "$conf"
+done
+
 exec /init
